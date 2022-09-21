2f33a8b2db3478051489a7e1625518feab1977e3
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2022-09-21 09:30:31 -0400

    test(input): :white_check_mark: add and refactor tests
    Create macro for tests and add new ones

--- src/lib/input/request.test.ts
+++ src/lib/input/request.test.ts
@@ -1,7 +1,116 @@
 import test from "ava";
-import { CanvasConnection } from "./request";
+import { gql } from "graphql-request";
+import { z } from "zod";
+import { CanvasConnection, CanvasQuerySchema } from "./request";
 
-test("Canvas endpoint returns data", async (t) => {
-  const assertion = await new CanvasConnection().call();
-  t.assert(assertion, "The Canvas endpoint is not working as expected!");
+interface ConTestParams {
+  schema: CanvasQuerySchema<z.Schema>;
+  errorMessage: string;
+  display?: boolean;
+}
+
+const connectionTest = test.macro(async (t, params: ConTestParams) => {
+  const { schema, errorMessage, display } = params;
+  const response = await new CanvasConnection(schema).call();
+  if (display !== undefined && display) console.log(response);
+  t.assert(response, errorMessage);
+});
+
+test("Canvas returns data", connectionTest, {
+  schema: {
+    req: gql`
+      query ReturnDataQ {
+        allCourses {
+          _id
+          courseCode
+          name
+        }
+      }
+    `,
+    val: z.object({
+      allCourses: z.array(
+        z.object({
+          _id: z.string(),
+          courseCode: z.string().nullable(),
+          name: z.string(),
+        })
+      ),
+    }),
+  },
+  errorMessage: "Canvas endpoint did not return data properly!",
+  display: true,
+});
+
+test("Canvas returns schema types", connectionTest, {
+  schema: {
+    req: gql`
+      query SchemaFragQ {
+        __schema {
+          types {
+            name
+          }
+        }
+      }
+    `,
+    val: z.object({
+      __schema: z.object({
+        types: z.array(
+          z.object({
+            name: z.string(),
+          })
+        ),
+      }),
+    }),
+  },
+  errorMessage: "Canvas endpoint did not return schema types correctly!",
+});
+
+test("Canvas returns fragment of docs", connectionTest, {
+  schema: {
+    req: gql`
+      query SchemaDocsFragQ {
+        __type(name: "__Schema") {
+          name
+          description
+          fields {
+            name
+            type {
+              name
+              kind
+              description
+              ofType {
+                name
+                kind
+                description
+              }
+            }
+          }
+        }
+      }
+    `,
+    val: z.object({
+      __type: z.object({
+        name: z.string(),
+        description: z.string().nullable(),
+        fields: z.array(
+          z.object({
+            name: z.string(),
+            type: z.object({
+              name: z.string().nullable(),
+              kind: z.string(),
+              description: z.string().nullable(),
+              ofType: z
+                .object({
+                  name: z.string().nullable(),
+                  kind: z.string(),
+                  description: z.string().nullable(),
+                })
+                .nullable(),
+            }),
+          })
+        ),
+      }),
+    }),
+  },
+  errorMessage: "Canvas endpoint did not return documentation correctly!",
 });
