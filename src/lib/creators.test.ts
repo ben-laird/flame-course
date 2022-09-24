23d3fa685db0e359a770de8252123004c2446143
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2022-09-24 18:06:16 -0400

    test: :white_check_mark: add creator tests
    Add course creator tests

---
+++ src/lib/creators.test.ts
@@ -0,0 +1,8 @@
+import test from "ava";
+import { courseCreator } from "./creators";
+
+test("Creator function returns data", async (t) => {
+  const courses = await courseCreator();
+  console.log(courses);
+  t.assert(courses, "Courses array contains no data!");
+});
