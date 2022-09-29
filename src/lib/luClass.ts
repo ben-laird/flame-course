ebdc5e376e122fba63e5e20a38b07a0ff9bffae1
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2022-09-29 15:49:58 -0400

    chore(objectSchema): :tada: implement luClass
    Fully implement LUClass as a TS class

--- src/lib/luClass.ts
+++ src/lib/luClass.ts
@@ -1,20 +1,44 @@
-// import OnlineCourse from "./onlineCourse";
-import Course from "./course";
+import Course, { CourseData } from "./course";
+
+export interface LUClassParams {
+  title: string;
+  subterm: LUClassSubterm;
+  lectureCourse: Course;
+  labCourse?: Course;
+}
+
+export interface LUClassData {
+  title: string;
+  subterm: LUClassSubterm;
+  courseLinks: {
+    lecture: CourseData;
+    lab?: CourseData;
+  };
+}
+
+export interface CourseLinks {
+  lecture: Course;
+  lab?: Course;
+}
+
+export type LUClassSubterm = "A" | "B" | "C" | "D" | "J";
 
 /**
  * A representation of an LU Class. An LU Class in this library is defined as a set of related courses,
  * whether in a lecture/lab duo, or something from Global Studies, idek what stuff Global Studies does lol.
  */
-export default interface LUClass {
+export default class LUClass {
   /**
    * The title of the class. Make it whatever you'd like!
    */
-  title: string;
-  /**
-   * The emoji that refers to this class.
-   * This is mostly for organization and can be safely ignored if emoji-coding isn't your style.
-   */
-  emojiCode?: string;
+  private _title: string;
+
+  public get title(): string {
+    return this._title;
+  }
+
+  private _subterm: LUClassSubterm;
+
   /**
    * The identifier of the subterm the course takes place in.
    *
@@ -27,22 +51,41 @@ export default interface LUClass {
    * Intensives will be in the J-term.
    * The J-term starts about a week before the A-term.
    */
-  subterm: "A" | "B" | "C" | "D" | "J";
+  public get subterm(): LUClassSubterm {
+    return this._subterm;
+  }
+
+  private _courseLinks: CourseLinks;
+
   /**
    * The set of courses that constitute the class.
-   * Generally, classes only consist of one course, but in the case of offerings with a lab component,
-   * the {@linkcode labCourse} will handle this data.
+   * Generally, classes only consist of one course,
+   * but in the case of offerings with a lab component,
+   * the lab property will handle this data.
    *
    * @note Online courses will never have extra components like lab.
    */
-  courses: {
-    /**
-     * The lecture component of the offering.
-     */
-    lectureCourse: Course; // | OnlineCourse;
-    /**
-     * The lab component of the offering, if any.
-     */
-    labCourse?: Course;
-  };
+  public get courses(): CourseLinks | undefined {
+    return this._courseLinks;
+  }
+
+  public get data(): LUClassData {
+    return {
+      title: this._title,
+      subterm: this._subterm,
+      courseLinks: {
+        lecture: this._courseLinks.lecture.data,
+        lab: this._courseLinks.lab?.data,
+      },
+    };
+  }
+
+  constructor(params: LUClassParams) {
+    ({ title: this._title, subterm: this._subterm } = params);
+
+    this._courseLinks = {
+      lecture: params.lectureCourse,
+      lab: params.labCourse,
+    };
+  }
 }
