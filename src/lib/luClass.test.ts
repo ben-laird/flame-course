f28e3282c2737b1614d51f06c658479bb8918c76
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2022-09-29 15:50:41 -0400

    test(objectSchema): :white_check_mark: add luClass tests
    Add testing for LUClass

---
+++ src/lib/luClass.test.ts
@@ -0,0 +1,93 @@
+import anyTest, { TestFn } from "ava";
+import Course from "./course";
+import LUClass, { CourseLinks, LUClassData } from "./luClass";
+import { ochemClassMock, ochemLabMock, ochemMock } from "./utils/testing";
+
+interface Subject {
+  class: LUClass;
+}
+
+interface GetterTesterParams {
+  getterToTest: keyof LUClass;
+  expectedVal: unknown;
+}
+
+const test = anyTest as TestFn<Subject>;
+
+/*
+=======================================
+Test Macros
+=======================================
+*/
+
+const getterTester = test.macro((t, params: GetterTesterParams) => {
+  const { getterToTest, expectedVal } = params;
+  const expected = expectedVal as typeof actual;
+  const actual = t.context.class[getterToTest];
+
+  t.deepEqual(
+    actual,
+    expected,
+    `${getterToTest} doesn't match what was expected!`
+  );
+});
+
+// Set up new LUClass instance before each test
+test.beforeEach((t) => {
+  t.context.class = new LUClass(ochemClassMock.input);
+});
+
+/*
+=======================================
+Getter Tests
+=======================================
+*/
+
+test("Title getter", getterTester, {
+  getterToTest: "title",
+  expectedVal: ochemClassMock.data.title,
+});
+
+test("Subterm getter", getterTester, {
+  getterToTest: "subterm",
+  expectedVal: ochemClassMock.data.subterm,
+});
+
+test("Courses getter", (t) => {
+  const actual = t.context.class.courses;
+  const expected: CourseLinks = {
+    lecture: new Course(ochemMock.input),
+    lab: new Course(ochemLabMock.input),
+  };
+
+  t.deepEqual(actual, expected, "Courses doesn't match what is expected!");
+});
+
+test("Class is constructed correctly", (t) => {
+  t.deepEqual(
+    t.context.class.data,
+    ochemClassMock.data,
+    "LU Class was not constructed correctly!"
+  );
+});
+
+test("Class without optionals constructed correctly", (t) => {
+  const subject = new LUClass({
+    ...ochemClassMock.input,
+    labCourse: undefined,
+  });
+
+  const expected: LUClassData = {
+    ...ochemClassMock.data,
+    courseLinks: {
+      lecture: ochemClassMock.data.courseLinks.lecture,
+      lab: undefined,
+    },
+  };
+
+  t.deepEqual(
+    subject.data,
+    expected,
+    "LU Class was not constructed correctly!"
+  );
+});
