f3ef527b4fac37dea6be837f86eecadf0d3d12bb
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2022-09-29 23:15:07 -0400

    chore(index): :building_construction: distribute exports
    Distribute export statements to other directories

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-export * from "./lib/objectSchema/assignment";
-export * from "./lib/objectSchema/course";
-export * from "./lib/objectSchema/luClass";
-export * from "./lib/objectSchema/module";
-export * from "./lib/objectSchema/semester";
+export * from "./lib/objectSchema";
