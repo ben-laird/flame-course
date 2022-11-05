13efa7f4d208aa8e1a147aa09b464f62062ff8c6
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2022-11-05 00:42:12 -0400

    refactor(input): :label: refactor and narrow types
    Refactor generic params, inferences, and narrow InputParams
    
    InputParams is now narrower, accepting a record of strings or numbers

--- src/lib/input/connection.ts
+++ src/lib/input/connection.ts
@@ -1,20 +1,32 @@
 import { z } from "zod";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type AnyConnection = Connection<any, any[], any, z.Schema>;
+export type AnyConnection = Connection<any[], any, z.Schema, any>;
 
 export type ConnectionInfer<
   T extends AnyConnection,
   U extends "inputParams" | "connectParams" | "val" | "output"
-> = T extends Connection<infer R1, infer R2, infer R3, infer R4>
-  ? { inputParams: R1; connectParams: R2; val: R3; output: R4 }[U]
+> = T extends Connection<
+  infer ConnectParams,
+  infer Output,
+  infer ZVal,
+  infer InputParams
+>
+  ? {
+      inputParams: InputParams;
+      connectParams: ConnectParams;
+      val: ZVal;
+      output: Output;
+    }[U]
   : never;
 
+export type ConnectionInputParams = Record<string, string | number> | null;
+
 export default abstract class Connection<
-  InputParams,
-  ConnectParams extends [...unknown[]],
+  ConnectParams extends ReadonlyArray<unknown>,
   OutShape extends z.infer<ZVal>,
-  ZVal extends z.Schema
+  ZVal extends z.Schema,
+  InputParams extends ConnectionInputParams = null
 > {
   protected abstract connect: (...params: ConnectParams) => Promise<OutShape>;
 
