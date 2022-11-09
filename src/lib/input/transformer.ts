9fcffba0bb962212021fee16e1c45f742a3072cb
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2022-11-09 13:40:11 -0500

    docs(input): :memo: add docs
    Add documentation to Transformer

--- src/lib/input/transformer.ts
+++ src/lib/input/transformer.ts
@@ -1,11 +1,25 @@
 import { AnyConnection, ConnectionInfer } from "./connection";
 
+/**
+ * Transform data from a connection into a useable piece of data
+ */
 export default class Transformer<Return, Con extends AnyConnection> {
   constructor(
+    /**
+     * The connection to use
+     */
     private connection: Con,
+    /**
+     * The function to use when transforming incoming data. the `data` parameter is fully typed.
+     */
     private transform: (data: ConnectionInfer<Con, "output">) => Return
   ) {}
 
+  /**
+   * Create a data set by calling the connection and transforming the incoming data
+   * @param params parameters needed to call for data from the connection
+   * @returns a transformed data set using the `transform` function
+   */
   public create = async (params: ConnectionInfer<Con, "inputParams">) => {
     const res = await this.connection.call(params);
     return this.transform(res);
