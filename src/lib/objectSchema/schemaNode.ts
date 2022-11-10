77bd9ab6fb0931a416e785234bd4bd71deb2b266
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2022-11-10 08:42:52 -0500

    feat(objectSchema): :sparkles: add schemaNode
    Create schema node abstract to standardize object schema

---
+++ src/lib/objectSchema/schemaNode.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+type AnySchemaNode = SchemaNode<AnySchemaNode[], z.Schema>;
+
+export type SchemaNodeInfer<
+  T extends AnySchemaNode,
+  U extends "dependencies" | "validator" | "shape"
+> = T extends SchemaNode<infer Deps, infer ZVal, infer Shape>
+  ? { dependencies: Deps; validator: ZVal; shape: Shape }[U]
+  : never;
+
+export default abstract class SchemaNode<
+  Deps extends AnySchemaNode[],
+  ZVal extends z.Schema,
+  Shape extends z.infer<ZVal> = z.infer<ZVal>
+> {
+  constructor(
+    protected deps: Deps,
+    protected val: ZVal,
+    protected data: Shape
+  ) {
+    val.parse(data);
+  }
+}
