cd855cf14d7b2d69ea7c6a39061fd9eeb379d5a7
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2022-11-13 14:48:14 -0500

    refactor(objectSchema): :boom: remove val from SchemaNode
    Remove val property from SchemaNode class
    
    Requires fewer arguments for SchemaNode and SchemaLinkedNode

--- src/lib/objectSchema/schemaNode.ts
+++ src/lib/objectSchema/schemaNode.ts
@@ -1,21 +1,13 @@
-import { z } from "zod";
-
-type AnySchemaNode = SchemaNode<z.Schema>;
+type AnySchemaNode = SchemaNode<Record<string, unknown>>;
 
 export type SchemaNodeInfer<T extends AnySchemaNode> = T extends SchemaNode<
-  z.Schema,
   infer Shape
 >
   ? Shape
   : never;
 
-export abstract class SchemaNode<
-  ZVal extends z.Schema,
-  Shape extends z.infer<ZVal> = z.infer<ZVal>
-> {
-  constructor(protected val: ZVal, protected data: Shape) {
-    val.parse(data);
-  }
+export abstract class SchemaNode<Shape extends Record<string, unknown>> {
+  constructor(protected data: Shape) {}
 
   public get wrappedData() {
     return this.data;
@@ -27,15 +19,14 @@ type AnyDependencies = Record<string, AnySchemaNode[]>;
 // type AnySchemaLinkedNode = SchemaLinkedNode<AnyDependencies, z.Schema>;
 
 export abstract class SchemaLinkedNode<
-  Deps extends AnyDependencies,
-  ZVal extends z.Schema,
-  Shape extends z.infer<ZVal> = z.infer<ZVal>
-> extends SchemaNode<ZVal> {
-  constructor(protected deps: Deps, val: ZVal, data: Shape) {
-    super(val, data);
+  Shape extends Record<string, unknown>,
+  Deps extends AnyDependencies
+> extends SchemaNode<Shape> {
+  constructor(data: Shape, protected deps: Deps) {
+    super(data);
   }
 
-  public get wrappedData() {
+  public get wrappedDataAndDependencies() {
     const { data, dependencies } = this;
 
     return { data, dependencies };
