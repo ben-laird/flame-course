99e6f820aa324a625637c30e96556305ead09d71
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2022-11-14 17:49:21 -0500

    feat(mediators): :sparkles: add NodeMediator
    Add NodeMediator class

---
+++ src/lib/mediators/nodeMediator.ts
@@ -0,0 +1,26 @@
+import { UnaryVariadic } from "../../types/utils";
+import { AnySchemaNode } from "../objectSchema/schemaNodes";
+
+/**
+ * Transform data from a schema node into something else, like a different schema
+ */
+export default class NodeMediator<
+  Return,
+  Nodes extends UnaryVariadic<AnySchemaNode>
+> {
+  /**
+   * Transform data from a schema node into something else, like a different schema
+   * @param nodes the nodes to use when transforming
+   * @param mediator the function to use when transforming the given nodes. the `data` parameter is fully typed.
+   */
+  constructor(
+    private nodes: Nodes,
+    private mediator: (data: Nodes) => Return
+  ) {}
+
+  /**
+   * Create a new schema by using the given nodes and transforming the schema
+   * @returns a transformed schema using the `mediator` function
+   */
+  public create = () => this.mediator(this.nodes);
+}
