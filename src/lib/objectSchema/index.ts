cc9e7c961660c7e257f5707922a142d1bf08b6f7
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2022-11-14 20:12:04 -0500

    refactor(objectSchema): :fire: update exports
    Update exports

--- src/lib/objectSchema/index.ts
+++ src/lib/objectSchema/index.ts
@@ -1,4 +1,1 @@
-export { default as Course } from "./course";
-export * from "./course";
-export { default as LUClass } from "./luClass";
-export * from "./luClass";
+export * as Raw from "./rawModel";
